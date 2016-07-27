e8f47b813b79f622c75bf1b95a97dedebcb8d7d1
nathan.caron <nathan.caron@MAPF01DJ2HL32L>
2016-07-27 13:49:31 -0400

    Add project files.

---
+++ DirectoryWatcher/Form1.Designer.vb
@@ -0,0 +1,177 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.TableLayoutPanel3 = New System.Windows.Forms.TableLayoutPanel()
+        Me.CompareButton = New System.Windows.Forms.Button()
+        Me.StatusBox = New System.Windows.Forms.TextBox()
+        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+        Me.StartButton = New System.Windows.Forms.Button()
+        Me.StopButton = New System.Windows.Forms.Button()
+        Me.MyWatcher = New System.IO.FileSystemWatcher()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.TableLayoutPanel3.SuspendLayout()
+        Me.TableLayoutPanel2.SuspendLayout()
+        CType(Me.MyWatcher, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel3, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.StatusBox, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel2, 0, 0)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.32964!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 78.67036!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(835, 361)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'TableLayoutPanel3
+        '
+        Me.TableLayoutPanel3.ColumnCount = 1
+        Me.TableLayoutPanel3.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel3.Controls.Add(Me.CompareButton, 0, 0)
+        Me.TableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel3.Location = New System.Drawing.Point(420, 3)
+        Me.TableLayoutPanel3.Name = "TableLayoutPanel3"
+        Me.TableLayoutPanel3.RowCount = 2
+        Me.TableLayoutPanel3.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel3.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel3.Size = New System.Drawing.Size(412, 70)
+        Me.TableLayoutPanel3.TabIndex = 4
+        '
+        'CompareButton
+        '
+        Me.CompareButton.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.CompareButton.Location = New System.Drawing.Point(150, 6)
+        Me.CompareButton.Name = "CompareButton"
+        Me.CompareButton.Size = New System.Drawing.Size(111, 23)
+        Me.CompareButton.TabIndex = 0
+        Me.CompareButton.Text = "Force Updates"
+        Me.CompareButton.UseVisualStyleBackColor = True
+        '
+        'StatusBox
+        '
+        Me.StatusBox.BackColor = System.Drawing.SystemColors.Window
+        Me.TableLayoutPanel1.SetColumnSpan(Me.StatusBox, 2)
+        Me.StatusBox.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.StatusBox.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.StatusBox.Location = New System.Drawing.Point(3, 79)
+        Me.StatusBox.Multiline = True
+        Me.StatusBox.Name = "StatusBox"
+        Me.StatusBox.ReadOnly = True
+        Me.StatusBox.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.StatusBox.Size = New System.Drawing.Size(829, 279)
+        Me.StatusBox.TabIndex = 0
+        Me.StatusBox.TabStop = False
+        Me.StatusBox.Text = "Press Start to begin watching directory!"
+        Me.StatusBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.StatusBox.WordWrap = False
+        '
+        'TableLayoutPanel2
+        '
+        Me.TableLayoutPanel2.ColumnCount = 1
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel2.Controls.Add(Me.StartButton, 0, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.StopButton, 0, 1)
+        Me.TableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel2.Location = New System.Drawing.Point(3, 3)
+        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
+        Me.TableLayoutPanel2.RowCount = 2
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel2.Size = New System.Drawing.Size(411, 70)
+        Me.TableLayoutPanel2.TabIndex = 3
+        '
+        'StartButton
+        '
+        Me.StartButton.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.StartButton.Location = New System.Drawing.Point(88, 6)
+        Me.StartButton.Name = "StartButton"
+        Me.StartButton.Size = New System.Drawing.Size(234, 23)
+        Me.StartButton.TabIndex = 0
+        Me.StartButton.Text = "Start"
+        Me.StartButton.UseVisualStyleBackColor = True
+        '
+        'StopButton
+        '
+        Me.StopButton.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.StopButton.Enabled = False
+        Me.StopButton.Location = New System.Drawing.Point(88, 41)
+        Me.StopButton.Name = "StopButton"
+        Me.StopButton.Size = New System.Drawing.Size(234, 23)
+        Me.StopButton.TabIndex = 1
+        Me.StopButton.Text = "Stop"
+        Me.StopButton.UseVisualStyleBackColor = True
+        '
+        'MyWatcher
+        '
+        Me.MyWatcher.EnableRaisingEvents = True
+        Me.MyWatcher.Filter = "*.xlsx"
+        Me.MyWatcher.Path = "Y:\WCS\PTR Phase Eternity"
+        Me.MyWatcher.SynchronizingObject = Me
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "PTR Watcher"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(835, 361)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "PTR Watcher"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.TableLayoutPanel3.ResumeLayout(False)
+        Me.TableLayoutPanel2.ResumeLayout(False)
+        CType(Me.MyWatcher, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents StatusBox As TextBox
+    Friend WithEvents MyWatcher As IO.FileSystemWatcher
+    Friend WithEvents TableLayoutPanel2 As TableLayoutPanel
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents TableLayoutPanel3 As TableLayoutPanel
+    Friend WithEvents CompareButton As Button
+    Friend WithEvents StartButton As Button
+    Friend WithEvents StopButton As Button
+End Class
---
+++ DirectoryWatcher/Form1.vb
@@ -0,0 +1,109 @@
+﻿Imports System
+Imports System.IO
+Imports Microsoft.VisualBasic
+Imports System.Security.Permissions
+
+
+
+Public Class Form1
+    Dim myFileCompare As New FileCompare
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        NotifyIcon1.Icon = Icon
+
+    End Sub
+
+    Private Sub StartButton_Click(sender As Object, e As EventArgs) Handles StartButton.Click
+        StatusBox.Text = "Now watching for changes..." & vbCrLf
+        StartButton.Enabled = False
+        StopButton.Enabled = True
+        MyWatcher.EnableRaisingEvents = True
+        'Run()
+    End Sub
+
+    Private Sub StopButton_Click(sender As Object, e As EventArgs) Handles StopButton.Click
+        StatusBox.Text = "Click Start to begin watching again!" & vbCrLf
+        StartButton.Enabled = True
+        StopButton.Enabled = False
+        MyWatcher.EnableRaisingEvents = False
+    End Sub
+
+    Private Sub MyWatcher_Renamed(sender As Object, e As RenamedEventArgs) Handles MyWatcher.Renamed
+        If e.Name.Contains("xlsx") Then
+            Dim copyFrom As String = e.FullPath
+            Dim copyTo As String = "\\alliance2.gd-ais.com\DavWWWRoot\sites\3\WCS\WCS Shared Documents IPT WCS Software\TI14 APB15\Workarea\RAN PTR Effort\" & e.Name
+            StatusBox.AppendText("Time: " & Date.Now & vbCrLf)
+            StatusBox.AppendText("File changed: " & e.Name & vbCrLf)
+            Try
+                File.Delete(copyTo)
+                File.Copy(copyFrom, copyTo)
+            Catch ex As Exception
+                MsgBox("An error has occurred trying to copy file to Alliance!")
+            End Try
+        End If
+    End Sub
+
+    Private Sub CompareButton_Click(sender As Object, e As EventArgs) Handles CompareButton.Click
+        Dim alliance As String = "\\alliance2.gd-ais.com\DavWWWRoot\sites\3\WCS\WCS Shared Documents IPT WCS Software\TI14 APB15\Workarea\RAN PTR Effort\"
+        Dim yDrive As String = "Y:\WCS\PTR Phase Eternity\"
+        If Directory.Exists(alliance) AndAlso Directory.Exists(yDrive) Then
+            Dim dirY As New DirectoryInfo(yDrive)
+            Dim listY = dirY.GetFiles("*PTR*.xlsx", SearchOption.TopDirectoryOnly)
+            For Each Cfile In listY
+                If Cfile.Name = "" Or Cfile.Name.Contains("$") Then
+                    Continue For
+                End If
+                File.Delete(alliance & Cfile.Name)
+                File.Copy(yDrive & Cfile.Name, alliance & Cfile.Name)
+            Next
+
+        End If
+
+    End Sub
+
+    Private Function md5(ByVal file_name As String) As String
+        'Dim MSXML As MSXML2.DOMDocument = New MSXML2.DOMDocument
+        Dim fileBytes As Byte() = File.ReadAllBytes(file_name)
+        Dim shaM As New Security.Cryptography.SHA512Managed()
+        Dim shaB As Byte() = shaM.ComputeHash(fileBytes)
+
+        Dim sBuilder As New System.Text.StringBuilder()
+        Dim i As Integer
+        For i = 0 To shaB.Length - 1
+            sBuilder.Append(shaB(i).ToString("x2"))
+        Next
+        Return sBuilder.ToString()
+    End Function
+
+    Private Sub NotifyIcon1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+        If WindowState = FormWindowState.Minimized Then
+            Show()
+            WindowState = FormWindowState.Normal
+        End If
+    End Sub
+
+    Private Sub Form1_Resize(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Resize
+        If WindowState = FormWindowState.Minimized Then
+            Hide()
+        End If
+    End Sub
+
+    Public Class FileCompare
+        Implements Collections.Generic.IEqualityComparer(Of System.IO.FileInfo)
+
+        Public Function Equals1(x As FileInfo, y As FileInfo) As Boolean Implements IEqualityComparer(Of FileInfo).Equals
+            If x.Name = y.Name Then
+                Return True
+            Else
+                Return False
+            End If
+        End Function
+
+        Public Function GetHashCode1(obj As FileInfo) As Integer Implements IEqualityComparer(Of FileInfo).GetHashCode
+            Dim s As String = obj.Name
+            Return s.GetHashCode()
+        End Function
+    End Class
+End Class
+
+
+
