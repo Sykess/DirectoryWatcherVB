411aed181a463f57664728f7dfded9ad4e37c07e
nathan.caron <nathan.caron@noreply.github.com>
2016-07-28 17:08:21 -0400

    yeah

--- DirectoryWatcher/Form1.Designer.vb
+++ DirectoryWatcher/Form1.Designer.vb
@@ -33,10 +33,19 @@ Partial Class Form1
         Me.StopButton = New System.Windows.Forms.Button()
         Me.MyWatcher = New System.IO.FileSystemWatcher()
         Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveLogToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveLogAsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.OptionsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DisableForceUpdates = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DisableStopButton = New System.Windows.Forms.ToolStripMenuItem()
         Me.TableLayoutPanel1.SuspendLayout()
         Me.TableLayoutPanel3.SuspendLayout()
         Me.TableLayoutPanel2.SuspendLayout()
         CType(Me.MyWatcher, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'TableLayoutPanel1
@@ -48,13 +57,13 @@ Partial Class Form1
         Me.TableLayoutPanel1.Controls.Add(Me.StatusBox, 1, 0)
         Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel2, 0, 0)
         Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 28)
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
         Me.TableLayoutPanel1.RowCount = 2
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.32964!))
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 78.67036!))
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
-        Me.TableLayoutPanel1.Size = New System.Drawing.Size(835, 361)
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(835, 333)
         Me.TableLayoutPanel1.TabIndex = 0
         '
         'TableLayoutPanel3
@@ -68,13 +77,13 @@ Partial Class Form1
         Me.TableLayoutPanel3.RowCount = 2
         Me.TableLayoutPanel3.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
         Me.TableLayoutPanel3.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.TableLayoutPanel3.Size = New System.Drawing.Size(412, 70)
+        Me.TableLayoutPanel3.Size = New System.Drawing.Size(412, 65)
         Me.TableLayoutPanel3.TabIndex = 4
         '
         'CompareButton
         '
         Me.CompareButton.Anchor = System.Windows.Forms.AnchorStyles.None
-        Me.CompareButton.Location = New System.Drawing.Point(150, 6)
+        Me.CompareButton.Location = New System.Drawing.Point(150, 4)
         Me.CompareButton.Name = "CompareButton"
         Me.CompareButton.Size = New System.Drawing.Size(111, 23)
         Me.CompareButton.TabIndex = 0
@@ -87,12 +96,12 @@ Partial Class Form1
         Me.TableLayoutPanel1.SetColumnSpan(Me.StatusBox, 2)
         Me.StatusBox.Dock = System.Windows.Forms.DockStyle.Fill
         Me.StatusBox.ImeMode = System.Windows.Forms.ImeMode.Disable
-        Me.StatusBox.Location = New System.Drawing.Point(3, 79)
+        Me.StatusBox.Location = New System.Drawing.Point(3, 74)
         Me.StatusBox.Multiline = True
         Me.StatusBox.Name = "StatusBox"
         Me.StatusBox.ReadOnly = True
         Me.StatusBox.ScrollBars = System.Windows.Forms.ScrollBars.Both
-        Me.StatusBox.Size = New System.Drawing.Size(829, 279)
+        Me.StatusBox.Size = New System.Drawing.Size(829, 256)
         Me.StatusBox.TabIndex = 0
         Me.StatusBox.TabStop = False
         Me.StatusBox.Text = "Press Start to begin watching directory!"
@@ -111,13 +120,13 @@ Partial Class Form1
         Me.TableLayoutPanel2.RowCount = 2
         Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
         Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.TableLayoutPanel2.Size = New System.Drawing.Size(411, 70)
+        Me.TableLayoutPanel2.Size = New System.Drawing.Size(411, 65)
         Me.TableLayoutPanel2.TabIndex = 3
         '
         'StartButton
         '
         Me.StartButton.Anchor = System.Windows.Forms.AnchorStyles.None
-        Me.StartButton.Location = New System.Drawing.Point(88, 6)
+        Me.StartButton.Location = New System.Drawing.Point(88, 4)
         Me.StartButton.Name = "StartButton"
         Me.StartButton.Size = New System.Drawing.Size(234, 23)
         Me.StartButton.TabIndex = 0
@@ -128,7 +137,7 @@ Partial Class Form1
         '
         Me.StopButton.Anchor = System.Windows.Forms.AnchorStyles.None
         Me.StopButton.Enabled = False
-        Me.StopButton.Location = New System.Drawing.Point(88, 41)
+        Me.StopButton.Location = New System.Drawing.Point(88, 37)
         Me.StopButton.Name = "StopButton"
         Me.StopButton.Size = New System.Drawing.Size(234, 23)
         Me.StopButton.TabIndex = 1
@@ -147,13 +156,72 @@ Partial Class Form1
         Me.NotifyIcon1.Text = "PTR Watcher"
         Me.NotifyIcon1.Visible = True
         '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.OptionsToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(835, 28)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SaveLogToolStripMenuItem, Me.SaveLogAsToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(44, 24)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'SaveLogToolStripMenuItem
+        '
+        Me.SaveLogToolStripMenuItem.Enabled = False
+        Me.SaveLogToolStripMenuItem.Name = "SaveLogToolStripMenuItem"
+        Me.SaveLogToolStripMenuItem.Size = New System.Drawing.Size(181, 26)
+        Me.SaveLogToolStripMenuItem.Text = "Save Log"
+        '
+        'SaveLogAsToolStripMenuItem
+        '
+        Me.SaveLogAsToolStripMenuItem.Enabled = False
+        Me.SaveLogAsToolStripMenuItem.Name = "SaveLogAsToolStripMenuItem"
+        Me.SaveLogAsToolStripMenuItem.Size = New System.Drawing.Size(181, 26)
+        Me.SaveLogAsToolStripMenuItem.Text = "Save Log As..."
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.Filter = "Text File|*.txt|All files|*.*"
+        Me.SaveFileDialog1.OverwritePrompt = False
+        '
+        'OptionsToolStripMenuItem
+        '
+        Me.OptionsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DisableForceUpdates, Me.DisableStopButton})
+        Me.OptionsToolStripMenuItem.Name = "OptionsToolStripMenuItem"
+        Me.OptionsToolStripMenuItem.Size = New System.Drawing.Size(73, 24)
+        Me.OptionsToolStripMenuItem.Text = "Options"
+        '
+        'DisableForceUpdates
+        '
+        Me.DisableForceUpdates.CheckOnClick = True
+        Me.DisableForceUpdates.Name = "DisableForceUpdates"
+        Me.DisableForceUpdates.Size = New System.Drawing.Size(324, 26)
+        Me.DisableForceUpdates.Text = "Disable Force Updates Confirmation"
+        '
+        'DisableStopButton
+        '
+        Me.DisableStopButton.CheckOnClick = True
+        Me.DisableStopButton.Name = "DisableStopButton"
+        Me.DisableStopButton.Size = New System.Drawing.Size(324, 26)
+        Me.DisableStopButton.Text = "Disable Stop Button Confirmation"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(835, 361)
         Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Controls.Add(Me.MenuStrip1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
         Me.Text = "PTR Watcher"
         Me.TableLayoutPanel1.ResumeLayout(False)
@@ -161,7 +229,10 @@ Partial Class Form1
         Me.TableLayoutPanel3.ResumeLayout(False)
         Me.TableLayoutPanel2.ResumeLayout(False)
         CType(Me.MyWatcher, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -174,4 +245,12 @@ Partial Class Form1
     Friend WithEvents CompareButton As Button
     Friend WithEvents StartButton As Button
     Friend WithEvents StopButton As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveLogToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveLogAsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents OptionsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DisableForceUpdates As ToolStripMenuItem
+    Friend WithEvents DisableStopButton As ToolStripMenuItem
 End Class
--- DirectoryWatcher/Form1.vb
+++ DirectoryWatcher/Form1.vb
@@ -1,7 +1,4 @@
-﻿Imports System
-Imports System.IO
-Imports Microsoft.VisualBasic
-Imports System.Security.Permissions
+﻿Imports System.IO
 
 
 
@@ -9,11 +6,15 @@ Public Class Form1
     Dim myFileCompare As New FileCompare
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         NotifyIcon1.Icon = Icon
+        SaveFileDialog1.InitialDirectory = Application.StartupPath
+        SaveFileDialog1.FileName = "DisplayLog" & Date.Now.Month & Date.Now.Day & ".txt"
 
     End Sub
 
     Private Sub StartButton_Click(sender As Object, e As EventArgs) Handles StartButton.Click
-        StatusBox.Text = "Now watching for changes..." & vbCrLf
+        StatusBox.Text = "Now watching for changes, started at " & Date.Now & vbCrLf
+        SaveLogAsToolStripMenuItem.Enabled = True
+        SaveLogToolStripMenuItem.Enabled = True
         StartButton.Enabled = False
         StopButton.Enabled = True
         MyWatcher.EnableRaisingEvents = True
@@ -21,9 +22,17 @@ Public Class Form1
     End Sub
 
     Private Sub StopButton_Click(sender As Object, e As EventArgs) Handles StopButton.Click
+        If DisableStopButton.Checked = False Then
+            Dim answer = MessageBox.Show("Would you like to Save the log?", "Stop Action", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+            If answer = MsgBoxResult.Yes Then
+                SaveLogAsToolStripMenuItem_Click(sender, e)
+            End If
+        End If
         StatusBox.Text = "Click Start to begin watching again!" & vbCrLf
         StartButton.Enabled = True
         StopButton.Enabled = False
+        SaveLogAsToolStripMenuItem.Enabled = False
+        SaveLogToolStripMenuItem.Enabled = False
         MyWatcher.EnableRaisingEvents = False
     End Sub
 
@@ -37,7 +46,7 @@ Public Class Form1
                 File.Delete(copyTo)
                 File.Copy(copyFrom, copyTo)
             Catch ex As Exception
-                MsgBox("An error has occurred trying to copy file to Alliance!")
+                StatusBox.AppendText("Error copying file " & e.Name & vbCrLf)
             End Try
         End If
     End Sub
@@ -46,16 +55,32 @@ Public Class Form1
         Dim alliance As String = "\\alliance2.gd-ais.com\DavWWWRoot\sites\3\WCS\WCS Shared Documents IPT WCS Software\TI14 APB15\Workarea\RAN PTR Effort\"
         Dim yDrive As String = "Y:\WCS\PTR Phase Eternity\"
         If Directory.Exists(alliance) AndAlso Directory.Exists(yDrive) Then
-            Dim dirY As New DirectoryInfo(yDrive)
-            Dim listY = dirY.GetFiles("*PTR*.xlsx", SearchOption.TopDirectoryOnly)
-            For Each Cfile In listY
-                If Cfile.Name = "" Or Cfile.Name.Contains("$") Then
-                    Continue For
+            CompareButton.Enabled = False
+            If DisableForceUpdates.Checked = False Then
+                Dim answer = MessageBox.Show("Are you sure you want to overwrite Alliance files?", "Confirm Overwrite", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+                If answer = MsgBoxResult.No Then
+                    CompareButton.Enabled = True
+                    Return
                 End If
-                File.Delete(alliance & Cfile.Name)
-                File.Copy(yDrive & Cfile.Name, alliance & Cfile.Name)
-            Next
-
+            End If
+            Try
+                Dim dirY As New DirectoryInfo(yDrive)
+                Dim listY = dirY.GetFiles("*PTR*.xlsx", SearchOption.TopDirectoryOnly)
+                For Each Cfile In listY
+                    If Cfile.Name = "" Or Cfile.Name.Contains("$") Then
+                        Continue For
+                    End If
+                    File.Delete(alliance & Cfile.Name)
+                    File.Copy(yDrive & Cfile.Name, alliance & Cfile.Name)
+                Next
+                    MessageBox.Show("Alliance updated successfully")
+                Catch ex As Exception
+                    MsgBox("Error copying files")
+                Finally
+                    CompareButton.Enabled = True
+                End Try
+            Else
+                MessageBox.Show("Either the Alliance or the Y drive folder doesn't exist")
         End If
 
     End Sub
@@ -103,6 +128,33 @@ Public Class Form1
             Return s.GetHashCode()
         End Function
     End Class
+
+    Private Sub SaveLogToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveLogToolStripMenuItem.Click
+        Dim currentPath As String = ""
+        Try
+            'currentPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase)
+            'currentPath = New Uri(currentPath).LocalPath
+            currentPath = Application.StartupPath
+            currentPath += "DisplayLog" & Date.Now.Month & Date.Now.Day & ".txt"
+            My.Computer.FileSystem.WriteAllText(currentPath, StatusBox.Text & vbCrLf, True)
+            MessageBox.Show(currentPath & " Saved Successfully!")
+        Catch ex As Exception
+            MessageBox.Show("Error saving to " & currentPath)
+        End Try
+    End Sub
+
+    Private Sub SaveLogAsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveLogAsToolStripMenuItem.Click
+        Dim saveFile As String = ""
+        If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+            saveFile = SaveFileDialog1.FileName
+            Try
+                My.Computer.FileSystem.WriteAllText(saveFile, StatusBox.Text & vbCrLf, True)
+                MessageBox.Show(saveFile & " Saved Successfully!")
+            Catch ex As Exception
+                MessageBox.Show("Error saving to " & saveFile)
+            End Try
+        End If
+    End Sub
 End Class
 
 
